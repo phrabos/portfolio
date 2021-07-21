28ed99788760f020cc8a03be8e5c1d0bdc759576
patrick hrabos <phrabos@gmail.com>
2021-07-21 14:13:26 -0400

    css

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,14 @@ const pageStyles = {
 	fontFamily: '-apple-system, Roboto, sans-serif, serif',
 };
 const headingStyles = {
-	marginTop: 200,
+	marginTop: 0,
 	marginBottom: 0,
+	maxWidth: 320,
+};
+
+const sectionStyles = {
+	marginTop: 400,
+	marginBottom: 400,
 	maxWidth: 320,
 };
 
@@ -22,81 +28,87 @@ const IndexPage = () => {
 			<title>Home Page</title>
 			<Navbar />
 			<h1 style={headingStyles}>This will be my Gatsby portfolio site...</h1>
-			<h1 id="about" style={headingStyles}>
-				About
-			</h1>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-				varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
-				tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
-				facilisis. Donec molestie sagittis efficitur. Etiam nec interdum lacus.
-				Donec vel urna quis ex luctus tincidunt non sollicitudin sapien. Sed non
-				risus et metus pharetra scelerisque. Pellentesque habitant morbi
-				tristique senectus et netus et malesuada fames ac turpis egestas.
-				Vestibulum mattis urna mi, at vestibulum libero faucibus ac. Cras
-				ultricies nulla a quam interdum, ac porttitor metus laoreet. Donec eget
-				bibendum tellus, vitae laoreet lectus.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-				varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
-				tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
-				facilisis. Donec molestie sagittis efficitur. Etiam nec interdum lacus.
-				Donec vel urna quis ex luctus tincidunt non sollicitudin sapien. Sed non
-				risus et metus pharetra scelerisque. Pellentesque habitant morbi
-				tristique senectus et netus et malesuada fames ac turpis egestas.
-				Vestibulum mattis urna mi, at vestibulum libero faucibus ac. Cras
-				ultricies nulla a quam interdum, ac porttitor metus laoreet. Donec eget
-				bibendum tellus, vitae laoreet lectus.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-				varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
-				tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
-				facilisis. Donec molestie sagittis efficitur. Etiam nec interdum lacus.
-				Donec vel urna quis ex luctus tincidunt non sollicitudin sapien. Sed non
-				risus et metus pharetra scelerisque. Pellentesque habitant morbi
-				tristique senectus et netus et malesuada fames ac turpis egestas.
-				Vestibulum mattis urna mi, at vestibulum libero faucibus ac. Cras
-				ultricies nulla a quam interdum, ac porttitor metus laoreet. Donec eget
-				bibendum tellus, vitae laoreet lectus.
-			</p>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-				varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
-				tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
-				facilisis. Donec molestie sagittis efficitur. Etiam nec interdum lacus.
-				Donec vel urna quis ex luctus tincidunt non sollicitudin sapien. Sed non
-				risus et metus pharetra scelerisque. Pellentesque habitant morbi
-				tristique senectus et netus et malesuada fames ac turpis egestas.
-				Vestibulum mattis urna mi, at vestibulum libero faucibus ac. Cras
-				ultricies nulla a quam interdum, ac porttitor metus laoreet. Donec eget
-				bibendum tellus, vitae laoreet lectus.
-			</p>
-			<h1 style={headingStyles} id="projects">
-				Projects
-			</h1>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-				varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
-				tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
-				facilisis. Donec molestie sagittis efficitur. Etiam nec interdum lacus.
-				Donec vel urna quis ex luctus tincidunt non sollicitudin sapien. Sed non
-				risus et metus pharetra scelerisque. Pellentesque habitant morbi
-				tristique senectus et netus et malesuada fames ac turpis egestas.
-				Vestibulum mattis urna mi, at vestibulum libero faucibus ac. Cras
-				ultricies nulla a quam interdum, ac porttitor metus laoreet. Donec eget
-				bibendum tellus, vitae laoreet lectus.
-			</p>
-			<h1 id="contact" style={headingStyles}>
-				Contact
-			</h1>
-			<form onSubmit={() => console.log('works')}>
-				<input placeholder="name" />
-				<input placeholder="email" />
-				<input placeholder="message" />
-				<button>submit</button>
-			</form>
+			<section style={sectionStyles}>
+				<h1 id="about" style={headingStyles}>
+					About
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+					varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
+					tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
+					facilisis. Donec molestie sagittis efficitur. Etiam nec interdum
+					lacus. Donec vel urna quis ex luctus tincidunt non sollicitudin
+					sapien. Sed non risus et metus pharetra scelerisque. Pellentesque
+					habitant morbi tristique senectus et netus et malesuada fames ac
+					turpis egestas. Vestibulum mattis urna mi, at vestibulum libero
+					faucibus ac. Cras ultricies nulla a quam interdum, ac porttitor metus
+					laoreet. Donec eget bibendum tellus, vitae laoreet lectus.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+					varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
+					tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
+					facilisis. Donec molestie sagittis efficitur. Etiam nec interdum
+					lacus. Donec vel urna quis ex luctus tincidunt non sollicitudin
+					sapien. Sed non risus et metus pharetra scelerisque. Pellentesque
+					habitant morbi tristique senectus et netus et malesuada fames ac
+					turpis egestas. Vestibulum mattis urna mi, at vestibulum libero
+					faucibus ac. Cras ultricies nulla a quam interdum, ac porttitor metus
+					laoreet. Donec eget bibendum tellus, vitae laoreet lectus.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+					varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
+					tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
+					facilisis. Donec molestie sagittis efficitur. Etiam nec interdum
+					lacus. Donec vel urna quis ex luctus tincidunt non sollicitudin
+					sapien. Sed non risus et metus pharetra scelerisque. Pellentesque
+					habitant morbi tristique senectus et netus et malesuada fames ac
+					turpis egestas. Vestibulum mattis urna mi, at vestibulum libero
+					faucibus ac. Cras ultricies nulla a quam interdum, ac porttitor metus
+					laoreet. Donec eget bibendum tellus, vitae laoreet lectus.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+					varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
+					tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
+					facilisis. Donec molestie sagittis efficitur. Etiam nec interdum
+					lacus. Donec vel urna quis ex luctus tincidunt non sollicitudin
+					sapien. Sed non risus et metus pharetra scelerisque. Pellentesque
+					habitant morbi tristique senectus et netus et malesuada fames ac
+					turpis egestas. Vestibulum mattis urna mi, at vestibulum libero
+					faucibus ac. Cras ultricies nulla a quam interdum, ac porttitor metus
+					laoreet. Donec eget bibendum tellus, vitae laoreet lectus.
+				</p>
+			</section>
+			<section style={sectionStyles}>
+				<h1 style={headingStyles} id="projects">
+					Projects
+				</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+					varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
+					tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
+					facilisis. Donec molestie sagittis efficitur. Etiam nec interdum
+					lacus. Donec vel urna quis ex luctus tincidunt non sollicitudin
+					sapien. Sed non risus et metus pharetra scelerisque. Pellentesque
+					habitant morbi tristique senectus et netus et malesuada fames ac
+					turpis egestas. Vestibulum mattis urna mi, at vestibulum libero
+					faucibus ac. Cras ultricies nulla a quam interdum, ac porttitor metus
+					laoreet. Donec eget bibendum tellus, vitae laoreet lectus.
+				</p>
+			</section>
+			<section style={sectionStyles}>
+				<h1 id="contact" style={headingStyles}>
+					Contact
+				</h1>
+				<form onSubmit={() => console.log('works')}>
+					<input placeholder="name" />
+					<input placeholder="email" />
+					<input placeholder="message" />
+					<button>submit</button>
+				</form>
+			</section>
 		</main>
 	);
 };
