63239944ddb4790ae068808a98bdf6bb46e2ed68
Patrick Hrabos <phrabos@gmail.com>
2021-07-21 21:56:31 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,50 +8,15 @@ const IndexPage = () => {
 		<>
 			<Navbar />
 			<div className="wrapper">
-				<h1 style={{fontSize: "4rem"}}>Patrick Hrabos</h1>
-				<h2 style={{fontSize: "2rem"}}>Software engineer passionate about the process of coding, from planning to deployment and learning along the way.</h2>
+				<h1 style={{fontSize: "8vw"}}>Patrick Hrabos</h1>
+				<h2 style={{fontSize: "4vw"}}>Software engineer passionate about the process of coding, from planning to deployment and learning along the way.</h2>
 				<h1 id="about">About</h1>
 				<p>
-					I am a software engineer with a background crunching numbers and raising capital as a skilled and accomplished accountant where I gained valuable business experience and problem-solving skills.  My passion for coding began as a kid, writing basic 					websites in HTML. I am excited to be able to merge my business knowledge into a career as a software engineer. Outside of work, I 				am an avid runner and by grinding through ultra-endurance feats I have learned lessons in perseverance, discipline, and dedication 				which I have used to take on challenging projects in the workforce. I am on a never-ending quest to learn everything. 
+					I am a software engineer with a background crunching numbers and raising capital as a skilled and accomplished accountant where I gained valuable business experience and problem-solving skills.  My passion for coding began as a kid, writing basic 					websites in HTML. I am excited to merge my business knowledge into a career as a software engineer. Outside of work, I 				am an avid runner and by grinding through ultra-endurance feats I have learned lessons in perseverance, discipline, and dedication 				which I have used to take on challenging projects in the workforce. I am on a never-ending quest to learn everything. 
 
 				</p>
 				<img className="bio-photo" src="https://avatars.githubusercontent.com/u/73199651?s=400&u=d36d92e05c949994580702381836a52f7e512eb1&v=4"/>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-					varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
-					tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
-					facilisis. Donec molestie sagittis efficitur. Etiam nec interdum
-					lacus. Donec vel urna quis ex luctus tincidunt non sollicitudin
-					sapien. Sed non risus et metus pharetra scelerisque. Pellentesque
-					habitant morbi tristique senectus et netus et malesuada fames ac
-					turpis egestas. Vestibulum mattis urna mi, at vestibulum libero
-					faucibus ac. Cras ultricies nulla a quam interdum, ac porttitor metus
-					laoreet. Donec eget bibendum tellus, vitae laoreet lectus.
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-					varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
-					tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
-					facilisis. Donec molestie sagittis efficitur. Etiam nec interdum
-					lacus. Donec vel urna quis ex luctus tincidunt non sollicitudin
-					sapien. Sed non risus et metus pharetra scelerisque. Pellentesque
-					habitant morbi tristique senectus et netus et malesuada fames ac
-					turpis egestas. Vestibulum mattis urna mi, at vestibulum libero
-					faucibus ac. Cras ultricies nulla a quam interdum, ac porttitor metus
-					laoreet. Donec eget bibendum tellus, vitae laoreet lectus.
-				</p>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
-					varius tellus sit amet sagittis. In nec elit sodales, ornare nunc sed,
-					tristique leo. Donec non lacus sem. Morbi rutrum nec purus non
-					facilisis. Donec molestie sagittis efficitur. Etiam nec interdum
-					lacus. Donec vel urna quis ex luctus tincidunt non sollicitudin
-					sapien. Sed non risus et metus pharetra scelerisque. Pellentesque
-					habitant morbi tristique senectus et netus et malesuada fames ac
-					turpis egestas. Vestibulum mattis urna mi, at vestibulum libero
-					faucibus ac. Cras ultricies nulla a quam interdum, ac porttitor metus
-					laoreet. Donec eget bibendum tellus, vitae laoreet lectus.
-				</p>
+
 				<h1 id="projects">Projects</h1>
 				<h3>Socket Jockey</h3>
 				<img src="https://jakethrasher.netlify.app/assets/socketjockey.png"/>
