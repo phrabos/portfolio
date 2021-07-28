e834d51749a5aa0e88ad273393ebcc731610186e
patrick hrabos <phrabos@gmail.com>
2021-07-28 09:07:13 -0400

    add to project descriptions

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,13 @@ const IndexPage = () => {
 					Collaboratively composing music with physics and the collision of time
 					and space. Users can choose a solo or collaborative room where they
 					drag and drop sonically charged shapes and control the sounds by
-					adjusting the physics engine.
+					adjusting the physics engine. <br />
+					This projecet was completed as a team of eight people working remotely
+					over a two-week sprint. My contributions were setting up the Socket.io
+					server, pair-programming the chat feature using Socket.io to
+					synchronize state on all related clients, pair-programmig the control
+					panel state management, and pair-programming the logic for state
+					managment on the server.
 				</Project>
 				<Project
 					name="Shame B0t"
@@ -33,7 +39,9 @@ const IndexPage = () => {
 				>
 					A Discord bot designed to increase productivity by restricting access
 					to chat channels for a user-specified time limit. Built remotely with
-					a team of four people over a four-day sprint.
+					a team of four people over a four-day sprint. My contributions to the
+					project were coding the countdown timer commands and logic, the exit
+					command, and functions to change the user's nickname.
 				</Project>
 				<Project
 					name="Taurus Trading"
@@ -41,7 +49,8 @@ const IndexPage = () => {
 					alt="taurus-trading-logo"
 				>
 					Taurus Trading is a mock portfolio and stock market research app that
-					implements a live social media feed to gauge market sentiment. I coded
+					implements a live social media feed to gauge market sentiment. Built
+					remotely with a team of four people over a four-day sprint. I coded
 					the full-stack social media feature in addition to the functions and
 					rendering of the trending stocks feature.
 				</Project>
@@ -54,7 +63,8 @@ const IndexPage = () => {
 					preferred diet type. We provide tabular data and bar charts built with
 					CanvasJS to help the user meet their daily macronutrient goals. Built
 					remotely with a team of four people over a four-day sprint using
-					vanilla JavaScript.
+					vanilla JavaScript. I coded the modals, the food log page and the
+					feature to add a custom food to the data saved in local storage.
 				</Project>
 				<Contact />
 			</main>
