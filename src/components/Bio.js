41435e53c352035f815ce2ce10b954ee6292b9f8
patrick hrabos <phrabos@gmail.com>
2021-07-28 08:37:09 -0400

    css bio section

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,11 +6,11 @@ export const Bio = () => {
 		<section className="bio-wrapper">
 			<p>
 				I have a background raising capital as a skilled and accomplished
-				accountant. I gained valuable business experience and teamwork skills by
-				solving the daily challenges of growth that start-up companies often
-				face. I have a well-rounded approach to timely solving big problems by
-				breaking them down into small, manageable pieces and being diligent
-				throughout the process.
+				accountant where I gained valuable business experience and teamwork
+				skills by solving the daily challenges of growth that start-up companies
+				often face. I have a well-rounded approach to timely solving big
+				problems by breaking them down into small, manageable pieces and being
+				diligent throughout the process.
 				<br />
 				<br />
 				When I am not coding I like to run long distances, observe nature,
@@ -20,7 +20,7 @@ export const Bio = () => {
 			<img
 				className="bio-photo"
 				src="https://avatars.githubusercontent.com/u/73199651?s=400&u=d36d92e05c949994580702381836a52f7e512eb1&v=4"
-				alt=""
+				alt="portrait"
 			/>
 		</section>
 	);
