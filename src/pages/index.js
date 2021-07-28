6c1dcd8102ce0a1e5277daa14f577ce154b46364
patrick hrabos <phrabos@gmail.com>
2021-07-28 09:42:55 -0400

    add assets to images directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,10 @@ import { Navbar } from '../components/Navbar';
 import { Project } from '../components/Project';
 import { Top } from '../components/Top';
 import '../styles/global.css';
+import socket from '../images/socket-jockey.png';
+import nutrizone from '../images/nutrizone.png';
+import taurus from '../images/taurus-trading.png';
+import shamebot from '../images/shame-bot.png';
 
 const IndexPage = () => {
 	return (
@@ -16,11 +20,7 @@ const IndexPage = () => {
 				<Header id="about">About</Header>
 				<Bio />
 				<Header id="projects">Projects</Header>
-				<Project
-					name="Socket Jockey"
-					image="https://jakethrasher.netlify.app/assets/socketjockey.png"
-					alt="socket-jockey-screenshot"
-				>
+				<Project name="Socket Jockey" image={socket} alt="socket jockey">
 					Collaboratively composing music with physics and the collision of time
 					and space. Users can choose a solo or collaborative room where they
 					drag and drop sonically charged shapes and control the sounds by
@@ -32,33 +32,21 @@ const IndexPage = () => {
 					panel state management, and pair-programming the logic for state
 					managment on the server.
 				</Project>
-				<Project
-					name="Shame B0t"
-					image="https://via.placeholder.com/500x350"
-					alt="shame-bot-logo"
-				>
+				<Project name="Shame B0t" image={shamebot} alt="shame bot">
 					A Discord bot designed to increase productivity by restricting access
 					to chat channels for a user-specified time limit. Built remotely with
 					a team of four people over a four-day sprint. My contributions to the
 					project were coding the countdown timer commands and logic, the exit
 					command, and functions to change the user's nickname.
 				</Project>
-				<Project
-					name="Taurus Trading"
-					image="https://via.placeholder.com/500x350"
-					alt="taurus-trading-logo"
-				>
+				<Project name="Taurus Trading" image={taurus} alt="taurus trading">
 					Taurus Trading is a mock portfolio and stock market research app that
 					implements a live social media feed to gauge market sentiment. Built
 					remotely with a team of four people over a four-day sprint. I coded
 					the full-stack social media feature in addition to the functions and
 					rendering of the trending stocks feature.
 				</Project>
-				<Project
-					name="Nutrizone"
-					image="https://jakethrasher.netlify.app/assets/nutrizone.png"
-					alt="nutrizone-screenshot"
-				>
+				<Project name="Nutrizone" image={nutrizone} alt="nutrizone">
 					Nutrizone helps users track their daily caloric intake based on their
 					preferred diet type. We provide tabular data and bar charts built with
 					CanvasJS to help the user meet their daily macronutrient goals. Built
