ad08d70867400c8fa292f11e845588a9d7c0570f
patrick hrabos <phrabos@gmail.com>
2021-07-28 10:15:51 -0400

    add github logo and project links

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../styles/project.css';
+import githubLogo from '../images/github.png';
 
-export const Project = ({ children, image, name, alt }) => {
+export const Project = ({ children, image, name, alt, link, github }) => {
 	return (
 		<>
 			<h3>{name}</h3>
-			<img className="project-image" src={image} alt={alt} />
+			<a href={link} target="_blank">
+				<img className="project-image" src={image} alt={alt} />
+			</a>
+			<a href={github} target="_blank">
+				<img className="github-logo" src={githubLogo} alt="github logo" />
+			</a>
 			<p>{children}</p>
 		</>
 	);
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,13 @@ const IndexPage = () => {
 				<Header id="about">About</Header>
 				<Bio />
 				<Header id="projects">Projects</Header>
-				<Project name="Socket Jockey" image={socket} alt="socket jockey">
+				<Project
+					name="Socket Jockey"
+					image={socket}
+					alt="socket jockey"
+					link="https://socketjockey.netlify.app/"
+					github="https://github.com/socket-jockey"
+				>
 					Collaboratively composing music with physics and the collision of time
 					and space. Users can choose a solo or collaborative room where they
 					drag and drop sonically charged shapes and control the sounds by
@@ -32,21 +38,39 @@ const IndexPage = () => {
 					panel state management, and pair-programming the logic for state
 					managment on the server.
 				</Project>
-				<Project name="Shame B0t" image={shamebot} alt="shame bot">
+				<Project
+					name="Shame B0t"
+					image={shamebot}
+					alt="shame bot"
+					link="https://github.com/Shame-B0t"
+					github="https://github.com/Shame-B0t"
+				>
 					A Discord bot designed to increase productivity by restricting access
 					to chat channels for a user-specified time limit. Built remotely with
 					a team of four people over a four-day sprint. My contributions to the
 					project were coding the countdown timer commands and logic, the exit
 					command, and functions to change the user's nickname.
 				</Project>
-				<Project name="Taurus Trading" image={taurus} alt="taurus trading">
+				<Project
+					name="Taurus Trading"
+					image={taurus}
+					alt="taurus trading"
+					link="https://taurus-trading.netlify.app/"
+					github="https://github.com/taurus-trading"
+				>
 					Taurus Trading is a mock portfolio and stock market research app that
 					implements a live social media feed to gauge market sentiment. Built
 					remotely with a team of four people over a four-day sprint. I coded
 					the full-stack social media feature in addition to the functions and
 					rendering of the trending stocks feature.
 				</Project>
-				<Project name="Nutrizone" image={nutrizone} alt="nutrizone">
+				<Project
+					name="Nutrizone"
+					image={nutrizone}
+					alt="nutrizone"
+					link="https://nutri-zone.netlify.app"
+					github="https://github.com/Dominique-Sakrisson/nutri-project"
+				>
 					Nutrizone helps users track their daily caloric intake based on their
 					preferred diet type. We provide tabular data and bar charts built with
 					CanvasJS to help the user meet their daily macronutrient goals. Built
