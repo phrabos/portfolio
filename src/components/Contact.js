e6e5894b0dfdd34f1b35ab4a2700199cd6d9b14e
patrick hrabos <phrabos@gmail.com>
2021-09-07 16:09:32 -0700

    style form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,13 +7,15 @@ import '../styles/form.css';
 export const Contact = () => {
 	return (
 		<section className="contact-wrapper">
-			<Header id="contact">Contact</Header>
-			<form onSubmit={() => console.log('works')}>
-				<input type="email" placeholder="email" />
-				<input type="text" placeholder="name" />
-				<textarea placeholder="message..." rows="5" cols="35" />
-				<Button>Submit</Button>
-			</form>
+			<div className="inner-container">
+				<Header id="contact">Contact</Header>
+				<form onSubmit={() => console.log('works')}>
+					<input type="email" placeholder="email" />
+					<input type="text" placeholder="name" />
+					<textarea placeholder="message..." rows="6" cols="35" />
+					<Button>Submit</Button>
+				</form>
+			</div>
 		</section>
 	);
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,5 +2,9 @@ import React from 'react';
 import '../styles/global.css';
 
 export const Header = ({ children, id }) => {
-	return <h1 id={id}>{children}</h1>;
+	return (
+		<h1 style={{ fontSize: '2em' }} id={id}>
+			{children}
+		</h1>
+	);
 };
